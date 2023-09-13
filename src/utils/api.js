9d5b4b661a231a6dc92a47396bc598dfc1be9ad8
Mohammed <sajidcurious9@gmail.com>
2023-09-14 00:47:21 +0530

    added fetch function along with headers and params

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,18 @@
 import axios from "axios";
 
-const BASE_URL = "";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+const TMBD_TOKEN = import.meta.env.local.VITE_APP_TMBD_TOKEN;
+
+const headers = {
+  Authorization: "bearer " + TMBD_TOKEN,
+};
+
+export const fetchDataFromApi = async (url, params) => {
+  try {
+    const { data } = await axios.get(BASE_URL + url, { headers, params });
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
