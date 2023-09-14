5be831757ae65b6d188339bd05133a3b3c3688f1
Mohammed <sajidcurious9@gmail.com>
2023-09-15 00:02:47 +0530

    used redux provider and store in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { store } from ".store/store";
 import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
