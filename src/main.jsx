7f1f7f99807b784b6dafab17acf8ae421f5d30f3
Mohammed <sajidcurious9@gmail.com>
2023-09-15 03:34:06 +0530

    imported store.js

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.scss";
-import { store } from ".store/store";
 import { Provider } from "react-redux";
+import store from "./store/store.js";
+store;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
