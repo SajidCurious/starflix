14e2b090875794db040b36a86f82e1150cfb9ca2
Mohammed <sajidcurious9@gmail.com>
2023-09-27 01:45:32 +0530

    edited

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 import "./style.scss";
 
 const HeroBanner = () => {
-  return <div>HeroBanner</div>;
+  const [background, setBackground] = useState("");
+  const [query, setQuery] = useState("");
+
+  return (
+    <div className="heroBanner">
+      <div className="wrapper">
+        <div className="heroBannerContent">
+          <span className="title">Welcome.</span>
+          <span className="subTitle">
+            Millions of movies, TV shows and people to discover. Explore now.
+          </span>
+          <div className="searchInput">
+            <input
+              type="text"
+              placeholder="Search for a movie or tv show...."
+            />
+            <button>Search</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HeroBanner;
