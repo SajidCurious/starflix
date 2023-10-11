ddf1614546638411a58d3b62e89370017ea75f99
Mohammed <sajidcurious9@gmail.com>
2023-10-12 02:16:09 +0530

    building trending section

---
+++ src/pages/home/trending/Trending.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+
+const Trending = () => {
+  return <div>Trending</div>;
+};
+
+export default Trending;
