430ee678cccaeb4ec565e42c0ef9069b6ed5c5ac
Mohammed <sajidcurious9@gmail.com>
2023-10-12 03:59:40 +0530

    completed this component

--- src/components/genres/Genres.jsx
+++ src/components/genres/Genres.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
-const Genres = () => {
-  return <div>Genres</div>;
+import "./style.scss";
+
+const Genres = ({ data }) => {
+  const { genres } = useSelector((state) => state.home);
+
+  return (
+    <div className="genres">
+      {data?.map((g) => {
+        if (!genres[g]?.name) return;
+        return (
+          <div key={g} className="genre">
+            {genres[g]?.name}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Genres;
