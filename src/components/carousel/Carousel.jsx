285b4d4927858097a417b5eff4279879b6d3a398
Mohammed <sajidcurious9@gmail.com>
2023-10-12 03:18:29 +0530

    building carousel and imported required items

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,4 +1,19 @@
-import React from "react";
+import React, { useRef } from "react";
+import {
+  BsFillArrowLeftCircleFill,
+  BsFillArrowRightCircleFill,
+} from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import dayjs from "dayjs";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+import Img from "../lazyLoadImage/Img";
+import PosterFallback from "../../assets/no-poster.png";
+import CircleRating from "../circleRating/CircleRating";
+import Genres from "../genres/Genres";
+
+import "./style.scss";
 
 const Carousel = () => {
   return <div>Carousel</div>;
