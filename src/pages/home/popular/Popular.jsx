e614184fa3e7bd4b26b36c317b2af684beb90f09
Mohammed <sajidcurious9@gmail.com>
2023-10-12 21:40:32 +0530

    edited

--- src/pages/home/popular/Popular.jsx
+++ src/pages/home/popular/Popular.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
+
+import Carousel from "../../../components/carousel/Carousel";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
 
 import useFetch from "../../../hooks/useFetch";
-import Carousel from "../../../components/carousel/Carousel";
 
 const Popular = () => {
   const [endpoint, setEndpoint] = useState("movie");
@@ -11,7 +12,7 @@ const Popular = () => {
   const { data, loading } = useFetch(`/${endpoint}/popular`);
 
   const onTabChange = (tab) => {
-    setEndpoint(tab === "Movie" ? "movie" : "tv");
+    setEndpoint(tab === "Movies" ? "movie" : "tv");
   };
 
   return (
@@ -20,7 +21,7 @@ const Popular = () => {
         <span className="carouselTitle">What's Popular</span>
         <SwitchTabs data={["Movies", "TV Shows"]} onTabChange={onTabChange} />
       </ContentWrapper>
-      <Carousel data={data?.results} loading={loading} />
+      <Carousel data={data?.results} loading={loading} endpoint={endpoint} />
     </div>
   );
 };
