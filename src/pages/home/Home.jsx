709aa0f73f851adcbc542976c54aed07f696c30f
Mohammed <sajidcurious9@gmail.com>
2023-10-12 21:51:45 +0530

    imported TopRated component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,14 +3,14 @@ import "./style.scss";
 import HeroBanner from "./heroBanner/HeroBanner";
 import Trending from "./trending/Trending";
 import Popular from "./popular/Popular";
-Popular;
+import TopRated from "./topRated/TopRated";
 const Home = () => {
   return (
     <div className="homePage">
       <HeroBanner />
       <Trending />
       <Popular />
-      <div style={{ height: 1000 }}></div>
+      <TopRated />
     </div>
   );
 };
