376e51a452acb969b02ea377d8cb99a116315da2
Mohammed <sajidcurious9@gmail.com>
2023-10-13 12:49:47 +0530

    modified

--- src/pages/details/cast/Cast.jsx
+++ src/pages/details/cast/Cast.jsx
@@ -4,8 +4,8 @@ import { useSelector } from "react-redux";
 import "./style.scss";
 
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
-import Img from "../../../components/lazyLoadImage/Img";
 import avatar from "../../../assets/avatar.png";
+import Img from "../../../components/lazyLoadImages/img";
 
 const Cast = ({ data, loading }) => {
   const { url } = useSelector((state) => state.home);
